34e09dd8bdfbe3c73e8bf1bf2820da31f7e9d381
R3ahim <r9ahim@gmail.com>
2022-02-15 03:08:55 +0600

    2 push

--- image/js.js
+++ image/js.js
@@ -2,4 +2,14 @@ document.getElementById('case-plus').addEventListener('click',function(){
     const caseInput = document.getElementById('case-numbers');
     const caseNumber = caseInput.value ;
    caseInput.value = parseInt(caseNumber) + 1;
+
+   // minues 
+
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+     
+      const caseInput = document.getElementById('case-numbers');
+      const caseNumber = caseInput.value;
+      caseInput.value = parseInt(caseNumber) -1;
+
 })
