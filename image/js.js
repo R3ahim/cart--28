aaa1beed158e8349c80c339d4ee6de12269c9861
R3ahim <r9ahim@gmail.com>
2022-02-15 15:45:25 +0600

    3push

--- image/js.js
+++ image/js.js
@@ -1,15 +1,34 @@
+
+function updateCaseNumber(product,isIncreseing,price){
+    let caseInput = document.getElementById(product + '-numbers');
+    let caseNumber = caseInput.value ;
+    if(isIncreseing == true){
+caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if(caseNumber >0){
+ caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+
+    // update totla
+  const  caseTotal = document.getElementById(product + '-total');
+  caseTotal.innerText = caseNumber * price;
+  
+}
+
+
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateCaseNumber('phone',true,1219)
+})
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateCaseNumber('phone',false,1219)
+})
+
 document.getElementById('case-plus').addEventListener('click',function(){
-    const caseInput = document.getElementById('case-numbers');
-    const caseNumber = caseInput.value ;
-   caseInput.value = parseInt(caseNumber) + 1;
-
-   // minues 
-
+    updateCaseNumber('case',true,59)
+ 
 })
 document.getElementById('case-minus').addEventListener('click',function(){
-     
-      const caseInput = document.getElementById('case-numbers');
-      const caseNumber = caseInput.value;
-      caseInput.value = parseInt(caseNumber) -1;
+     updateCaseNumber('case',false,59);
 
 })
